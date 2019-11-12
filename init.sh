025ff6cd15b10b1b556be950f80533e190949633
Alexey Elistratov <mialeter@gmail.com>
2019-11-12 20:25:08 +0300

    nginx conf change2

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-git init
-git config --global user.name %username%
-git config --global user.email %useremail%
-git clone https://github.com/ave-78/django-train.git
+#git init
+#git config --global user.name ave
+#git config --global user.email ave-78@mail.ru
+#git clone https://github.com/ave-78/django-train.git
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/conf.d/nginx.conf
